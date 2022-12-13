43e854712cf1e3b9a7a6c4f5944a4a1aaeea5710
Oksana Blokhina <oksa-blokhi@yandex.ru>
2022-12-13 21:05:35 +0300

    Task2

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,2 +1,11 @@
 # Напишите программу для. проверки истинности утверждения
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+
+x = int (input('Введите x: '))
+y = int (input('Введите y: '))
+z = int (input('Введите z: '))
+
+if (not (x or y or z)) ==(not x and not y and not z): True
+else: False
+print(True or False)
