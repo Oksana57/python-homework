dd1d9e8de8b47492abea96a91b2942e0711beee3
Oksana Blokhina <oksa-blokhi@yandex.ru>
2022-12-18 12:16:50 +0300

    Task5

--- HW2/Task5/Task5.py
+++ HW2/Task5/Task5.py
@@ -1,12 +1,6 @@
 # 5 Реализуйте алгоритм перемешивания списка.
 
-# void InversArray( int [] array)
-# {
-#     for(int i=0;i<array.Length; i++)
-#     {
-#         array[i] *=-1;
-#     }
-# }
+
 import random
 
 def rand_list(a,b,n):
